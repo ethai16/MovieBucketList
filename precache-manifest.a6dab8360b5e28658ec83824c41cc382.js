7b10e1b2cdd677fbc4bf820a2b94107c70b48016
Erick Thai <erick.thai16@gmail.com>
2023-08-30 13:06:57 -0500

    Updates

---
+++ precache-manifest.a6dab8360b5e28658ec83824c41cc382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbeca9d2e8515e1ae9d9",
+    "url": "./static/css/main.251f1a5c.chunk.css"
+  },
+  {
+    "revision": "cbeca9d2e8515e1ae9d9",
+    "url": "./static/js/main.cbeca9d2.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "ce4317d3605742ea0274",
+    "url": "./static/js/2.ce4317d3.chunk.js"
+  },
+  {
+    "revision": "aa6e782088e5871c1674c0b363543ea7",
+    "url": "./static/media/joshua-eckstein-1099605-unsplash.aa6e7820.jpg"
+  },
+  {
+    "revision": "033011ff531bd60d358a04db3c324745",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MoveieBucketList/precache-manifest.aa3cc6a0e45af641d9f0627ac6a41f0e.js"
+  "./precache-manifest.a6dab8360b5e28658ec83824c41cc382.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MoveieBucketList/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
